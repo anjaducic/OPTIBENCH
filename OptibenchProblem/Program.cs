2f2ac4a1aa52d36187081d9a65971ef3c1122f96
Anja Ducic <ducic.ra5.2020@uns.ac.rs>
2024-03-07 23:11:38 +0100

    feat: Optibench problem process created

---
+++ OptibenchProblem/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+     c.SwaggerDoc("v1", new OpenApiInfo { Title = "OptibenchProblem API", Description = "Testing OPTIBENCH Problem", Version = "v1" });
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+   c.SwaggerEndpoint("/swagger/v1/swagger.json", "OptibenchProcess API V1");
+});
+
+
+
+
+app.Run();
