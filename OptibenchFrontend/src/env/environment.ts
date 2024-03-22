66f54d3b2b550c796ebc80512b4ec906dcad4cea
Anja Ducic <ducic.ra5.2020@uns.ac.rs>
2024-03-22 23:26:22 +0100

    feat: Environment

---
+++ OptibenchFrontend/src/env/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    host: "https://localhost:5201/",
+    apiHost: "https://localhost:5201/api/",
+    imageHost: "https://localhost:5201/images/",
+    translateHost: "https://libretranslate.de/",
+};
